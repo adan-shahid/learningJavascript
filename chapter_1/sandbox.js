1d1fd850f5a59c30e4d39ce28295236e82ae149d
Adan <adanshahid.student@gmail.com>
2025-06-02 20:31:01 +0500

    studying basics of it

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,18 @@
+// let age = 20;
+// let year = 2025;
+// console.log(age, year);
+
+// age = 30;
+// console.log(age);
+
+// const points = 100; //THIS VARIABLE WILL NOT BE CHANGED LATER.
+
+// console.log(points);
+
+//STRING
+
+console.log("hello, world");
+
+let firstName = "Adan";
+let lastName = "Shahid";
+console.log(firstName +' '+ lastName);
